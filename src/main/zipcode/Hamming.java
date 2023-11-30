95cf198ff7dc199dfd959d8607ee9fc916b627b8
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-29 23:11:19 -0500

    hammingDistance method, need to create exception

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,23 @@
 package zipcode;
 
 public class Hamming {
+    String s;
+    String s1;
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        String[] sArr = s.split("");
+        String[] s1Arr = s1.split("");
+        int hamming = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(!sArr[i].equals(s1Arr[i])){
+                hamming++;
+            }
+        }
+        return hamming;
     }
 }
